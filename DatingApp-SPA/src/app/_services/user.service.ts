bc876c8435a13a4a9b6080cbd2f0f2ce70e5c5ab
dianastefan13 <stefan.diana.m@gmail.com>
2019-10-03 14:22:20 +0300

    End of section 10

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -18,12 +18,16 @@ export class UserService {
 
 constructor(private http: HttpClient) { }
 
-getUsers(): Observable<User[]>{
+getUsers(): Observable<User[]> {
   return this.http.get<User[]>(this.baseUrl + 'users');
 }
 
-getUser(id): Observable<User>{
+getUser(id): Observable<User> {
   return this.http.get<User>(this.baseUrl + 'users/' + id);
+}
+
+updateUser(id: number, user: User) {
+  return this.http.put(this.baseUrl + 'users/' + id, user);
 }
 
 }
